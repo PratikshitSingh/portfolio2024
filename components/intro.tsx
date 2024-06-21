c772fdf6be602797a3f9dea36c3526c6de5eb231
Pratikshit Singh <pratikshit.singh.eee16@itbhu.ac.in>
2024-06-21 17:11:38 -0500

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -86,7 +86,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:tranlate-x-1 transition" />
         </Link>
         <a
-          href="https://drive.google.com/file/d/159-hIUqwTZR7tNZ9-sMgDYbDxTt_icxR/view?usp=sharing"
+          href="https://drive.google.com/file/d/11X2QYqfdQFfBF_QEtoiaZXh-eAE5Fwlg/view?usp=sharing"
           target="_blank"
           className="group flex items-center bg-white px-7 py-3 gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack dark:bg-white/10"
         >
